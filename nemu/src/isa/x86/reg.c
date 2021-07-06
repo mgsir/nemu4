a2c923cf656fd254e78b9396c6ee272b11b4e4c7
tracer-ics2020 <tracer@njuics.org>
2021-07-06 10:04:49 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: d08a9fb1e05f4e76a21538efef401c0d     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  10:04:49 up 50 min,  1 user,  load average: 0.08, 0.16, 0.16

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -44,13 +44,13 @@ void reg_test() {
   //   vaddr_t pc;
 void isa_reg_display() {
   printf("------------------show all registers---------------\n");
-  printf("exa:%x\n",cpu.eax);
-  printf("ecx:%x\n",cpu.ecx);
-  printf("edx:%x\n",cpu.edx);
-  printf("esp:%x\n",cpu.esp);
-  printf("ebp:%x\n",cpu.ebp);
-  printf("esi:%x\n",cpu.esi);
-  printf("edi:%x\n",cpu.edi);
+  printf("exa:0x%x\n",cpu.eax);
+  printf("ecx:0x%x\n",cpu.ecx);
+  printf("edx:0x%x\n",cpu.edx);
+  printf("esp:0x%x\n",cpu.esp);
+  printf("ebp:0x%x\n",cpu.ebp);
+  printf("esi:0x%x\n",cpu.esi);
+  printf("edi:0x%x\n",cpu.edi);
   printf("pc:%x\n\n",cpu.pc);
 }
 
