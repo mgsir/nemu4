c9e8fee99f90a99a86598e11b59dcdc6d023799c
tracer-ics2020 <tracer@njuics.org>
2021-07-06 10:18:12 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 604ed1dbe59a431db58aa1d010ed0324     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  10:18:12 up 5 min,  1 user,  load average: 0.00, 0.01, 0.00

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -51,7 +51,7 @@ void isa_reg_display() {
   printf("ebp:0x%x\n",cpu.ebp);
   printf("esi:0x%x\n",cpu.esi);
   printf("edi:0x%x\n",cpu.edi);
-  printf("pc:%x\n\n",cpu.pc);
+  printf("pc: 0x%x\n\n",cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
