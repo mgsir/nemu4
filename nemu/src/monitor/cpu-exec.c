89edff911dd6de1b4ecd7bd641638da8632fac93
tracer-ics2020 <tracer@njuics.org>
2021-07-06 08:47:30 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: d941f564ab854393857fd3f66a91537f     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  08:47:30 up 17 min,  0 users,  load average: 0.22, 0.07, 0.05

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -75,6 +75,7 @@ void cpu_exec(uint64_t n) {
   uint64_t timer_start = get_time();
 
   for (; n > 0; n --) {
+    printf("%ld",n);
     vaddr_t this_pc = cpu.pc;
 
     /* Execute one instruction, including instruction fetch,
