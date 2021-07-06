26d23a3895dab4a131ff6a6d3b2c19724fae25e1
tracer-ics2020 <tracer@njuics.org>
2021-07-06 09:39:24 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: d08a9fb1e05f4e76a21538efef401c0d     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  09:39:24 up 25 min,  1 user,  load average: 0.70, 0.21, 0.12

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -28,7 +28,7 @@ static char* rl_gets() {
 }
 
 static int cmd_c(char *args) {
- if(args == NULL) printf("NULL\n");
+ //if(args == NULL) printf("NULL\n");
   cpu_exec(-1);
   return 0;
 }
@@ -49,7 +49,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  {"si %d","execute instructions ",cmd_si},
+  {"si","execute instructions ",cmd_si},
 
   /* TODO: Add more commands */
 
@@ -82,7 +82,10 @@ static int cmd_help(char *args) {
 
 static int cmd_si(char *args)
 {
-  return -1;
+  uint32_t n =  atoi(args);
+  for(int i = 0; i < n; ++i ) { isa_exec_once();}
+  
+  return 0;
 }
 
 void ui_mainloop() {
