b5d41cee9483c27007431ea3979fde7d0b4f0426
tracer-ics2020 <tracer@njuics.org>
2021-07-08 13:47:51 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 250bb7d7b3d24ed4bef974b513ecae49     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  13:47:51 up 58 min,  2 users,  load average: 0.01, 0.04, 0.04

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -191,10 +191,8 @@ uint32_t find_main_operator(uint32_t p, uint32_t q)
 }
 
 uint32_t eval(uint32_t p,  uint32_t q)
-{ for(int i = p; i <= q; ++i)
-    {
-      printf("%s",tokens[i].str);
-    }
+{ 
+ 
   if(p > q)  return 0;
   else if(p == q) return (uint32_t)strtol(tokens[p].str,NULL,10);
   else if(check_parentheses(p,q) == true) return eval(p+1,q-1);
@@ -202,7 +200,10 @@ uint32_t eval(uint32_t p,  uint32_t q)
   {
     uint32_t pos = find_main_operator(p,q);
     printf("sign: %c\n",tokens[pos].type);
-   
+    for(int i = p; i <= q; ++i)
+    {
+      printf("%s",tokens[i].str);
+    }
     switch (tokens[pos].type)
     {
       case '+':
