6f8e4b5a23e5e6e7ca39c89b8f9450b2b2cccbd9
tracer-ics2020 <tracer@njuics.org>
2021-07-13 09:47:04 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: aa40b56e62e2402e9f187a1764a62bd3     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  09:47:04 up  1:02,  3 users,  load average: 0.03, 0.01, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -209,23 +209,26 @@ uint32_t eval(uint32_t p,  uint32_t q)
   else if(p == q) return (uint32_t)strtol(tokens[p].str,NULL,10);
   else if(check_parentheses(p,q) == true) 
   {
+      /*
     printf("p:%u,q:%u\n",p,q);
     for (int i = p; i <= q; ++i)
     {
       printf("%s", tokens[i].str);
     }
-    printf("\n"); return eval(p + 1, q - 1);
+    */
+     return eval(p + 1, q - 1);
   }
   else
   {
     uint32_t pos = find_main_operator(p,q);
-    printf("sign: %c num:%d\n",tokens[pos].type,pos);
-    printf("p:%u,q:%u\n",p,q);
-    for(int i = p; i <= q; ++i)
+   // printf("sign: %c num:%d\n",tokens[pos].type,pos);
+    //printf("p:%u,q:%u\n",p,q);
+    /*for(int i = p; i <= q; ++i)
     {
       printf("%s",tokens[i].str);
     }
     printf("\n");
+    */
     switch (tokens[pos].type)
     {
       case '+':
