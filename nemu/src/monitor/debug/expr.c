c52c210dacc5a52ebb4154ebf3f34fae2725f150
tracer-ics2020 <tracer@njuics.org>
2021-07-13 16:26:12 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 67f7402c7a1c44db8f4cca58c3a972ba     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  16:26:12 up  1:55,  2 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -363,6 +363,7 @@ word_t expr(char *e, bool *success) {
     }
   }
 
+  /*
   for(uint32_t i = 0; i < nr_token;++i) 
   {
     if(tokens[i].type == TK_0X)
@@ -380,6 +381,7 @@ word_t expr(char *e, bool *success) {
         --nr_token;
     }
   }
+  */
     _dealwith_sepcial_sign(TK_0X);
   /* deal with '&' case */
  //  _dealwith_sepcial_sign(TK_$);
