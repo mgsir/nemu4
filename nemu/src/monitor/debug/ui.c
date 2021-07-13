da580f00ce795451e2bd95a356981084b01ff107
tracer-ics2020 <tracer@njuics.org>
2021-07-13 21:59:08 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 67f7402c7a1c44db8f4cca58c3a972ba     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  21:59:08 up  7:12,  5 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -160,7 +160,7 @@ static int cmd_w(char *args)
 
     if(wp == NULL) {
         wp = new_wp();
-        wp->next = NULL;
+        wp->next = 0;
         strcpy(wp->exp,args);
         wp->info = expr(args,&scuccess);
     }else{
