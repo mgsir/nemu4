3512141b35793757afce3b186f5b2c0cd2e130e4
tracer-ics2020 <tracer@njuics.org>
2021-07-14 09:33:11 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 67f7402c7a1c44db8f4cca58c3a972ba     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  09:33:11 up  8:07,  8 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -55,13 +55,13 @@ void free_wp(WP *wp, WP *head)
 WP* new_wp()
 {
     assert(free_ != NULL);
-    int temp_cnt = cnt - 1;
+    //int temp_cnt = cnt - 1;
     WP *removed_head = head;
     printf("%d\n",cnt);
 
     if(cnt == 0) {removed_head = &wp_pool[0]; removed_head->next = NULL;}
     else{
-        while(temp_cnt --){removed_head = removed_head->next;}
+        while(removed_head->next){removed_head = removed_head->next;}
         removed_head->next = &wp_pool[cnt];
         removed_head->next->next = NULL;
     }
