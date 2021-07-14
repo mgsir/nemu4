5bbec42caaaeb34e2e7d37ce900eb71002105670
tracer-ics2020 <tracer@njuics.org>
2021-07-14 10:04:37 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 67f7402c7a1c44db8f4cca58c3a972ba     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  10:04:37 up  8:38,  8 users,  load average: 0.04, 0.13, 0.08

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -9,13 +9,14 @@ static int cnt = 0;
 
 void init_wp_pool() {
     int i;
-    //if(free_ != NULL) return;
     for (i = 0; i < NR_WP; i ++) {
         wp_pool[i].NO = i;
         wp_pool[i].next = &wp_pool[i + 1];
         wp_pool[i].info = -1;
     }
     wp_pool[NR_WP - 1].next = NULL;
+    head = NULL;
+    free_ = wp_pool;
 /*
     head = NULL;
     free_ = wp_pool;
