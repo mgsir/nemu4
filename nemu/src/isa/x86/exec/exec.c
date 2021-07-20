eca93d8d0c7a308cd94b56a64036921c4b1c9e8f
tracer-ics2020 <tracer@njuics.org>
2021-07-20 17:20:03 +0800

    >  compile 191220000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 1c008b281d3f4216a952199ba553251b            Boot ID: 2320d9948ce144a6b6db4a02acf6b2c9     Virtualization: oracle   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-16-amd64       Architecture: x86-64  17:20:03 up  1:55,  1 user,  load average: 0.23, 0.15, 0.10

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -127,6 +127,7 @@ again:
     IDEX (0xf7, E, gp3)
     IDEXW(0xfe, E, gp4, 1)
     IDEX (0xff, E, gp5)
+    EX(0xc3,ret)
   case 0x66: s->isa.is_operand_size_16 = true; goto again;
   default: exec_inv(s);
   }
