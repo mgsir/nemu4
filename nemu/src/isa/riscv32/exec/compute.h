3dbc1aab4d1c18367e7b538f4d729dca2379f0df
tracer-ics2020 <tracer@njuics.org>
2021-08-17 23:56:23 +0800

    >  compile 191220000    Static hostname: debian1          Icon name: computer-vm            Chassis: vm         Machine ID: e16b947b077a44f886fe3d5552da2135            Boot ID: bf8324f3c20a48ccb357739cabd48ff8     Virtualization: oracle   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-8-amd64       Architecture: x86-64  23:56:23 up  9:36,  1 user,  load average: 0.02, 0.04, 0.00

--- nemu/src/isa/riscv32/exec/compute.h
+++ nemu/src/isa/riscv32/exec/compute.h
@@ -18,7 +18,7 @@ static inline def_EHelper(jal){
 
   rtl_addi(s,ddest,&cpu.pc,4);
   rtl_addi(s,&cpu.pc,&cpu.pc,id_src1->imm);
-  print_asm_template2(auipc);
+  print_asm_template2(jal);
 }
 
 static inline def_EHelper(jalr){
@@ -26,7 +26,7 @@ static inline def_EHelper(jalr){
   word_t t = cpu.pc + 4;
   rtl_addi(s,&cpu.pc,ddest,id_src1->imm);
   rtl_li(s,ddest,t);
-  print_asm_template2(auipc);
+  print_asm_template3(jalr);
 }
 
 
