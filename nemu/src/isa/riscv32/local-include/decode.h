90ef5c70d0d18efacf9ee99e0555372175374b19
tracer-ics2020 <tracer@njuics.org>
2021-08-18 16:46:11 +0800

    >  compile 191220000    Static hostname: debian1          Icon name: computer-vm            Chassis: vm         Machine ID: e16b947b077a44f886fe3d5552da2135            Boot ID: bf8324f3c20a48ccb357739cabd48ff8     Virtualization: oracle   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-8-amd64       Architecture: x86-64  16:46:11 up 16:35,  1 user,  load average: 0.47, 0.21, 0.07

--- nemu/src/isa/riscv32/local-include/decode.h
+++ nemu/src/isa/riscv32/local-include/decode.h
@@ -46,7 +46,7 @@ static inline def_DHelper(S) {
 }
 
 static inline def_DHelper(J) {
-  decode_op_i(s, id_src1, s->isa.instr.j.offset << 3, true);
+  decode_op_i(s, id_src1, 0xc, true);
   decode_op_r(s, id_dest, s->isa.instr.j.rd, false);
 }
 
