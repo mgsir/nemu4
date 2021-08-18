995888a3a41c8633bf9279c640f196587bfa7fda
tracer-ics2020 <tracer@njuics.org>
2021-08-18 22:17:36 +0800

    >  compile 191220000    Static hostname: debian1          Icon name: computer-vm            Chassis: vm         Machine ID: e16b947b077a44f886fe3d5552da2135            Boot ID: bf8324f3c20a48ccb357739cabd48ff8     Virtualization: oracle   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-8-amd64       Architecture: x86-64  22:17:36 up 22:06,  1 user,  load average: 0.04, 0.05, 0.07

--- nemu/src/isa/riscv32/local-include/decode.h
+++ nemu/src/isa/riscv32/local-include/decode.h
@@ -8,10 +8,10 @@
 static inline def_DopHelper(i) {
   op->type = OP_TYPE_IMM;
  
-  // if(val & 0x00100000){
-  //   val = val | 0xfff00000;
-  //   printf("123");
-  // }
+  if(val & 0x00100000){
+    val = val | 0xfff00000;
+    printf("123");
+  }
   op->imm = val;
   // printf(" : 0x%08x\n",(int)val);
   print_Dop(op->str, OP_STR_SIZE, "%d", op->imm);
