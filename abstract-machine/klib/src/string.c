9cda7f666e3dd938aaec1bd43993636f9d42521d
tracer-ics2020 <tracer@njuics.org>
2021-08-21 16:54:04 +0800

    >  run 191220000    Static hostname: debian1          Icon name: computer-vm            Chassis: vm         Machine ID: e16b947b077a44f886fe3d5552da2135            Boot ID: 3475ea43f9894f708e5016ad4fbd25af     Virtualization: oracle   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-8-amd64       Architecture: x86-64  16:54:04 up  4:37,  1 user,  load average: 0.07, 0.11, 0.10

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,31 +4,80 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  if(s == NULL) return 0;
+
+  size_t len = 0;
+  while(s[len] != '\0')
+  {
+    ++len;
+  }
+
+  return len;
+}
+char* strncpy(char* dst, const char* src, size_t n) {
+  size_t i = 0;
+  for( ; i < n && src[i] != '\0'; ++i) dst[i] = src[i];
+  for(; i < n; ++i) dst[i] = '\0';
+
+  return dst;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  return strncpy(dst,src,strlen(src));
 }
 
-char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
-}
+
 
 char* strcat(char* dst, const char* src) {
   return NULL;
 }
 
 int strcmp(const char* s1, const char* s2) {
+  
+  size_t len_s1 = strlen(s1);
+  size_t len_s2 = strlen(s2);
+  size_t min_len  = len_s1 < len_s2 ?  len_s1 : len_s2;
+
+  for(size_t i = 0; i < min_len; ++i)
+  {
+    if(s1[i] > s2[i]) return 1;
+    else if(s1[i] < s2[i]) return -1;
+  }
+
+  if(len_s1 > len_s2) return 1;
+  else if(len_s1 < len_s2) return -1; 
+
   return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  // uint32_t len_s1 = strlen(s1);
+  // uint32_t len_s2 = strlen(s2);
+  // int min_len  = len_s1 < len_s2 ?  len_s1 : len_s2;
+
+  // min_len = min_len > n ? n : min_len;
+
+  for(uint32_t i = 0; i < n; ++i)
+  {
+    if(s1[i] > s2[i]) return 1;
+    else if(s1[i] < s2[i]) return -1;
+  }
+
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  if(v == NULL) return NULL;
+  size_t i  = 0;
+
+  char * temp_v = v;
+  for( ; i < n; ++i)
+  {
+    temp_v[i] = c;
+  }
+ 
+ return v;
+
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
@@ -36,7 +85,12 @@ void* memmove(void* dst,const void* src,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  size_t i = 0;
+
+  char * temp_out = out;
+  const char * temp_in  = in;
+  for( ; i < n ; ++i) temp_out[i] = temp_in[i];
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
