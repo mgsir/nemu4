4677b673ae53c80bcfe029e1f492c5cf26b6d0f1
tracer-ics2020 <tracer@njuics.org>
2021-08-21 18:46:41 +0800

    >  run 191220000    Static hostname: debian1          Icon name: computer-vm            Chassis: vm         Machine ID: e16b947b077a44f886fe3d5552da2135            Boot ID: 3475ea43f9894f708e5016ad4fbd25af     Virtualization: oracle   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-8-amd64       Architecture: x86-64  18:46:41 up  6:29,  1 user,  load average: 0.07, 0.11, 0.09

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,58 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+
+  va_list ap;
+  char c = *fmt;
+
+  va_start(ap,fmt);
+  int print_cnt = 0;
+  int num;
+  char *str;
+
+  while(c)
+  {
+    c = *++fmt;
+    switch (c)
+    {
+    case 'd':
+      num = va_arg(ap,int);
+      if(num == 0) { putch('0'); ++print_cnt;}
+      else{
+        int num_len = 0;
+        int temp_num = num;
+        int arr_num[100];
+        while(temp_num)
+        {
+          temp_num /= 10;
+          arr_num[num_len] = temp_num % 10;
+          ++num_len;
+        }
+
+        for(size_t i = 0; i  < num_len ;++i)
+        {
+          putch(arr_num[i] - '0');
+          ++print_cnt;
+        }
+      }
+      break;
+    case 's':
+       str = va_arg(ap,char *);
+      for(size_t i = 0; i < strlen(str); ++i)
+      {
+        putch(str[i]);
+        ++print_cnt;
+      }
+      break;
+    default:
+      putch(c);
+      ++print_cnt;
+    }
+  }
+
+  va_end(ap);
+  
+  return print_cnt;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
